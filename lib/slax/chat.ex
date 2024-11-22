d39e62731f564398848ba54fe9be88079d8a0e1f
dattrungnam <datvtrg0510@gmail.com>
2024-11-22 17:07:15 +0700

    32. seeds

---
+++ lib/slax/chat.ex
@@ -0,0 +1,50 @@
+defmodule Slax.Chat do
+  alias Slax.Repo
+  alias Slax.Chat.{Room, Message}
+  import Ecto.Query
+
+  def get_first_room! do
+    Repo.one(from r in Room, limit: 1, order_by: [asc: :name])
+  end
+
+  def get_room!(id) do
+    Repo.get!(Room, id)
+  end
+
+  def list_rooms do
+    Room |> Repo.all()
+  end
+
+  def create_room(attrs) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_room(room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def change_room(room, attrs \\ %{}) do
+    Room.changeset(room, attrs)
+  end
+
+  def get_list_of_messages(room) do
+    Repo.all(
+      from m in Message,
+        where: m.room_id == ^room.id,
+        order_by: [asc: :inserted_at, desc: :id],
+        preload: [:room, :user]
+    )
+  end
+
+  def change_message(message, attrs \\ %{}) do
+    Message.changeset(message, attrs)
+  end
+
+  def create_message(attrs, user, room) do
+    %Message{user: user, room: room} |> Message.changeset(attrs) |> Repo.insert()
+  end
+end
---
+++ lib/slax/chat/room.ex
@@ -0,0 +1,26 @@
+defmodule Slax.Chat.Room do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Slax.Chat.Message
+
+  schema "rooms" do
+    field :name, :string
+    field :topic, :string
+    has_many :messages, Message
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(room, attrs) do
+    room
+    |> cast(attrs, [:name, :topic])
+    |> validate_required(:name)
+    |> validate_length(:name, max: 80)
+    |> validate_format(:name, ~r/\A[a-z0-9-]+\z/,
+      message: "can only contain lowercase letters, numbers and dashes"
+    )
+    |> validate_length(:topic, max: 200)
+    |> unsafe_validate_unique(:name, Slax.Repo)
+    |> unique_constraint(:name)
+  end
+end
